8a704447fa6dafa83834c504a23760b3cd92828f
Harold Calayan <haroldcalayan@gmail.com>
2022-01-04 09:11:10 +0800

    :sparkles: Implement tasks in MainActivity page

--- app/src/main/java/net/decenternet/technicalexam/ui/MainActivity.kt
+++ app/src/main/java/net/decenternet/technicalexam/ui/MainActivity.kt
@@ -1,20 +1,29 @@
 package net.decenternet.technicalexam.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.*
 import net.decenternet.technicalexam.R
+import net.decenternet.technicalexam.ui.tasks.TasksActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var ivBrandingLogo: ImageView? = null
+    private lateinit var ivBrandingLogo: ImageView
+    private lateinit var textViewQuote: TextView
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ivBrandingLogo = findViewById<View>(R.id.ivBrandingLogo) as ImageView
+        ivBrandingLogo = findViewById(R.id.ivBrandingLogo)
+        textViewQuote = findViewById(R.id.textViewQuote)
 
         /**
          * Tasks
@@ -22,9 +31,34 @@ class MainActivity : AppCompatActivity() {
          * 2. Set any image that best illustrate the quote to ivBrandingLogo.
          * 3. Display this screen for 3 seconds, then redirect to TasksActivity and close this MainActivity.
          */
+
+        ivBrandingLogo.setImageResource(R.drawable.sacrifice_today)
+        textViewQuote.setText(R.string.main_favorite_quote)
+
+        startTimer(SCREEN_LIFE)
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
+    private fun startTimer(time: Long) {
+        activityScope.launch {
+            delay(time)
+            finish()
+            openTasks()
+        }
+    }
+
+    private fun openTasks() {
+        val intent = Intent(this, TasksActivity::class.java)
+        startActivity(intent)
     }
 
     companion object {
-        var theCatalyst = true
+        const val SCREEN_LIFE = 3000L
+
+        var theCatalyst = false
     }
 }
